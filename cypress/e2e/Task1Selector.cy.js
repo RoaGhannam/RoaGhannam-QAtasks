b69c644f5ac27250ff2b8a8da2d481f176636484
RoaGhannam <roaghannam3@gmail.com>
2025-11-29 19:55:27 +0200

    Task1 Selectors

---
+++ cypress/e2e/Task1Selector.cy.js
@@ -0,0 +1,51 @@
+/// <reference.types="cypress" />
+
+
+describe('', () => {
+    it('validate that the user will be redirected to the correct page when click on conduit', () => {
+        cy.visit("https://demo.productionready.io/#/register")
+        cy.get(".navbar-brand").click()
+    });
+
+    describe('', () => {
+        it('validate that the user will be redirected to the correct page when click on Home', () => {
+            cy.visit("https://demo.productionready.io/#/register")
+            cy.contains("a","Home").click()
+        });
+    });
+
+    describe('', () => {
+       it('validate that the user will be redirected to the correct page when click on Sign up', () => {
+           cy.visit("https://demo.productionready.io/#/register")
+           cy.get('nav').find('a').contains('Sign up')
+       }); 
+    });
+    
+    describe('', () => {
+        it('', () => {
+            cy.visit("https://demo.productionready.io/#/register")
+            cy.contains("h1","Sign up")
+            cy.get(".logo-font")
+            cy.get(".attribution")
+        });
+    });
+    describe('', () => {
+        it('Validate that the user can sign up to the system', () => {
+            cy.visit("https://demo.productionready.io/#/register")
+            cy.get('input').eq(0).type('RoaGhannam')
+            cy.get('input').filter('[type="email"]').type('roaghannam@gmail.com')
+            cy.get('input[type="password"]').last().type('123456')
+            cy.get(".btn-lg").click()
+        
+        });
+    });
+
+    describe('', () => {
+        it('validate that the user will be redirected to the correct page when click on Have an account?', () => {
+            cy.visit("https://demo.productionready.io/#/register")
+            cy.contains('a', 'Have an account?').click();
+        });
+    });
+
+
+});
