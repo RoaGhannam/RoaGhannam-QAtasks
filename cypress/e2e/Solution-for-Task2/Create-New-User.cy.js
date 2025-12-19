445ba90ebe39018e3b43fc567de23d556230ca39
RoaGhannam <roaghannam3@gmail.com>
2025-12-19 16:52:02 +0200

    Update for task2

---
+++ cypress/e2e/Solution-for-Task2/Create-New-User.cy.js
@@ -0,0 +1,28 @@
+/// <reference.types="cypress" />
+
+describe('Create New User Account', () => {
+    it('Validate that a new user can successfully register using the "Signup / Login" page.', () => {
+        cy.visit('https://automationexercise.com/')
+        cy.get('.fa-lock').click()
+        cy.findByDataQa("signup-name").type("Roaagh")
+        cy.findByDataQa("signup-email").type("Roaagh@Gh.com")
+        cy.findByDataQa("signup-button").click()   
+        cy.get('#id_gender2').check()
+        cy.findByDataQa("password").type("1234")
+        cy.get('#days').select('29')
+        cy.get('#months').select('March')
+        cy.get('#years').select('2003')
+        cy.get('#newsletter').check()
+        cy.findByDataQa("first_name").type("Roa")
+        cy.findByDataQa("last_name").type("Ghannam")
+        cy.findByDataQa("company").type("Asal")
+        cy.findByDataQa("address").type("Tubas")
+        cy.get('#country').select('Israel')
+        cy.findByDataQa("state").type("single")
+        cy.findByDataQa("city").type("Tubas")
+        cy.findByDataQa("zipcode").type("123456")
+        cy.findByDataQa("mobile_number").type("0599147852")
+        cy.findByDataQa("create-account").click()   
+    });
+    
+});
