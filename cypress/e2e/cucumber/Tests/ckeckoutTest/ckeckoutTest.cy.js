df95d59aadef5229ae0845502cacc2ec987bc6a7
RoaGhannam <roaghannam3@gmail.com>
2026-01-13 20:36:01 +0200

    New For Task#3

--- cypress/e2e/cucumber/Tests/ckeckoutTest/ckeckoutTest.cy.js
+++ cypress/e2e/cucumber/Tests/ckeckoutTest/ckeckoutTest.cy.js
@@ -1,52 +1,52 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
 Given("The user navigate to the login page", () => {
-  cy.visit("https://www.saucedemo.com/");
+  cy.visit("https://www.saucedemo.com/")
 });
 
 Given("The user logs in with valid credentials", () => {
-  cy.get("#user-name").should("be.visible").type("standard_user");
-  cy.get("#password").should("be.visible").type("secret_sauce");
-  cy.get("#login-button").should("be.visible").click();
+  cy.get("#user-name").should("be.visible").type("standard_user")
+  cy.get("#password").should("be.visible").type("secret_sauce")
+  cy.get("#login-button").should("be.visible").click()
 });
 
 When("The user adds a product to the cart", () => {
-  cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
 });
 
 When("The user clicks on the cart icon", () => {
-  cy.get(".shopping_cart_link").click();
+  cy.get(".shopping_cart_link").click()
 });
 
 Then("The product should be added to the cart successfully", () => {
-  cy.get(".inventory_item_name").should("contain.text", "Sauce Labs Backpack");
+  cy.get(".inventory_item_name").should("contain.text", "Sauce Labs Backpack")
 });
 
 
 When("The user clicks on checkout button", () => {
-  cy.get('[data-test="checkout"]').click();
+  cy.get('[data-test="checkout"]').click()
 });
 
 When("The user enters first name in input field", () => {
-  cy.get('[data-test="firstName"]').type("Roa");
+  cy.get('[data-test="firstName"]').type("Roa")
 });
 
 When("The user enters last name in input field", () => {
-  cy.get('[data-test="lastName"]').type("Ghannam");
+  cy.get('[data-test="lastName"]').type("Ghannam")
 });
 
 When("The user enters postal code in input field", () => {
-  cy.get('[data-test="postalCode"]').type("12345");
+  cy.get('[data-test="postalCode"]').type("12345")
 });
 
 When("The user clicks on continue button", () => {
-  cy.get('[data-test="continue"]').click();
+  cy.get('[data-test="continue"]').click()
 });
 
 When("The user clicks on finish button", () => {
-  cy.get('[data-test="finish"]').click();
+  cy.get('[data-test="finish"]').click()
 });
 
 Then("The user should see the checkout complete page", () => {
-  cy.get(".complete-header").should("contain.text", "Thank you for your order!");
+  cy.get(".complete-header").should("contain.text", "Thank you for your order!")
 });
--- cypress/e2e/cucumber/Tests/loginTest/loginTest.cy.js
+++ cypress/e2e/cucumber/Tests/loginTest/loginTest.cy.js
@@ -1,23 +1,23 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
 Given("The user navigate to the login page", () => {
-  cy.visit("https://www.saucedemo.com/");
+  cy.visit("https://www.saucedemo.com/")
 });
 
 When("The user enter {string} in username field", (username) => {
-  cy.get("#user-name").should("be.visible").type(username);
+  cy.get("#user-name").should("be.visible").type(username)
 });
 
 When("The user enter {string} in password field", (password) => {
-  cy.get("#password").should("be.visible").type(password);
+  cy.get("#password").should("be.visible").type(password)
 });
 
 When("The user clicks on login button", () => {
-  cy.get("#login-button").should("be.visible").click();
+  cy.get("#login-button").should("be.visible").click()
 });
 
 Then("The user will be redirected to the Swag Labs page", () => {
-  cy.contains("Swag Labs").should("be.visible");
-  cy.url().should("eq", "https://www.saucedemo.com/inventory.html");     
-  cy.get("#react-burger-menu-btn").should("be.visible");
+  cy.contains("Swag Labs").should("be.visible")
+  cy.url().should("eq", "https://www.saucedemo.com/inventory.html")   
+  cy.get("#react-burger-menu-btn").should("be.visible")
 });
